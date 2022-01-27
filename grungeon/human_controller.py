28d8893b34ef90bc0341b0df98efae1645fb8d0b
martinballa <ballamartin95@gmail.com>
2022-01-27 11:50:18 +0000

    Added the human controller, it moves in the correct direction, but the sprite rendering is off - can add new keys later as we move on

---
+++ grungeon/human_controller.py
@@ -0,0 +1,52 @@
+import os
+from gym.utils import play
+
+from griddly import GymWrapperFactory, gd, GymWrapper
+# from griddly.RenderTools import VideoRecorder
+
+# todo movement is right but the sprite rotation is wrong
+KEYWORD_TO_KEY = {
+    (ord('d'), ): 1,
+    (ord('w'),): 2,
+    (ord('a'),): 3,
+    (ord('s'),): 4,
+}
+
+def callback(env):
+
+    initial_global_obs = env.render(observer=0, mode="rgb_array")
+    observation_shape = initial_global_obs.shape
+
+    # recorder = VideoRecorder()
+    # recorder.start("human_player_video_test.mp4", observation_shape)
+
+    def _callback(prev_obs, obs, action, rew, env_done, info):
+
+        global_obs = env.render(observer=0, mode="rgb_array")
+        # recorder.add_frame(global_obs)
+        if rew != 0:
+            print(f'\nReward: {rew}')
+        if env_done:
+            print(f'Done!')
+
+        if len(info) > 0:
+            print(info)
+
+    return _callback
+
+if __name__ == '__main__':
+    wrapper = GymWrapperFactory()
+    name = 'stochasticity_env'
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    yaml_path = os.path.abspath('../gdy/grungeon.yaml')
+
+    env = GymWrapper(yaml_path,
+                     player_observer_type=gd.ObserverType.SPRITE_2D,
+                     global_observer_type=gd.ObserverType.SPRITE_2D,
+                     image_path='../assets/',
+                     level=0)
+
+    env.reset()
+    global_visualization = env.render(observer='global', mode='rgb_array')
+
+    play.play(env, fps=10, keys_to_action=KEYWORD_TO_KEY, zoom=1)
